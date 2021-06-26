987ff3a369c494ee14098f3c6a7ca4c2ab1c3353
kotiev <salikotiev@mail.ru>
2021-06-26 11:00:13 +0300

    last comit

---
+++ js 2 ur/create-array.js
@@ -0,0 +1,10 @@
+// let a = [];
+// let two = [1,45, 5];
+// let three = ["5", "6", "7"];
+// let six = [4, 9, 44, "adam" , 'movsar", "aslan"];
+// let maygroup = [adam , movsar, mansur, aslan, abdulla, sali, bashir, amin, muhammad, hasan, alvi, yahya, ramzan, seda];
+// let f = [1, "zadacha", null, undefined, true, false];
+// let empty = [ [], [], []];
+// let double = [  [44, 77, 88], [22, 44, 76]  ];
+
+
---
+++ js 2 ur/edit-elements.js
@@ -0,0 +1,28 @@
+let characters = ["Рамси Болтон", "Тирион Ланнистер", "Станис Баратеон", "Теон Грейджой", "Эддард Старк", "Дайенерис Таргариен", "Джон Сноу"];
+const king = "Король Ночи";
+const stupid = "Бран Старк";
+
+characters[0] = king;
+/* A) измени первый элемент массива на значение переменной king. */
+console.log(characters);
+
+
+characters[1] = ('null');
+/* B) измени второй элемент массива на значение null */
+console.log(characters);
+
+
+characters[2] = ('Станис Король')
+/* C) измени третий элемент массива на строку "Станис Король". */
+console.log(characters);  
+
+
+characters[3] = stupid;
+/* D) измени четвертый элемент массива на значение переменной stupid.  */
+console.log(characters);
+
+
+
+characters[4] ="season", "01"
+/* E) измени пятый элемент массива на массив ["season", "01"].  */
+console.log(characters);
